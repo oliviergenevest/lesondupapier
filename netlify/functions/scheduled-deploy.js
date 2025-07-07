911091cce0bdb8c1200357c1bef6f01bff830cfb
oliviergenevest <olivier.genevest@gmail.com>
2025-07-07 14:46:33 +0200

    netlify.toml for function serror test

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -4,7 +4,7 @@ import { schedule } from '@netlify/functions';
 const BUILD_HOOK = 'https://api.netlify.com/build_hooks/6868efddfab5ec4fe79c0a4f';
 
 // Schedules the handler function to run “At 00:00.” (2h du matin en france)
-const handler = schedule('0 0 * * *', async () => {
+const handler = schedule('13 0 * * *', async () => {
   await fetch(BUILD_HOOK, {
     method: 'POST',
   }).then((response) => {
